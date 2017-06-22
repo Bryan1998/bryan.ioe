eecc4e8f710ae83b34e5cf2024a53ba3f395bb08
Bryan Hernandez <pdnbryan1998@gmail.com>
2017-06-22 08:59:52 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
-echo('This is a webpage');
-?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Page Title</title>
+<link rel="stylesheet" href="css/main.css" />
+</head>
+<body>
+
+<img src="img/logo.png" />
+<p>A very random number between 1 and 100 is <?php echo(mt_rand(0,100)); ?></p>
+
+</body>
+</html>
