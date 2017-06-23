103fa58bd5057c1591f659a0d2cefce8aa1ece5e
Bryan Hernandez <pdnbryan1998@gmail.com>
2017-06-23 08:52:24 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
-
+<html lang="en-US">
+	
 <head>
-	<title>Push The Win Button</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Bryan Hernandez</title>
 	<link rel="stylesheet" href="css/main.css" />
 </head>
 
 <body>
-	<img src="img/logo.png" />
+	<h1>Bryan Hernandez</h1>
 	<p><?php echo("A very random number between ".$min." and ".$max." is ".mt_rand($min,$max));?></p>
+	<br ?>
+	<a href="files/vmware-fix.txt">VMWare Fix</a>
 </body>
 
 </html>
